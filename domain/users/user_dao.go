65a1e60ce7ffdd858987e3a19381f15950d8e0d4
samirgattas <samirgattas@gmail.com>
2022-11-21 14:28:23 -0300

    Save users in the DB

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -2,7 +2,9 @@ package users
 
 import (
 	"fmt"
+	"strings"
 
+	"github.com/develop-microservices-in-go/bookstore_users-api/datasources/mysql/users_db"
 	"github.com/develop-microservices-in-go/bookstore_users-api/utils/date_utils"
 	"github.com/develop-microservices-in-go/bookstore_users-api/utils/errors"
 )
@@ -12,11 +14,19 @@ import (
  * Here, it is the logic to persist and get element from the DB. It is the access layer to the DB.
  */
 
+const (
+	indexUniqueEmail = "UNIQUE_email"
+	queryInsertUser  = "INSERT INTO users(first_name, last_name, email, date_created) VALUES(?,?,?,?);"
+)
+
 var (
 	userDB = make(map[int64]*User)
 )
 
 func (user *User) Get() *errors.RestErr {
+	if err := users_db.Client.Ping(); err != nil {
+		panic(err)
+	}
 	result := userDB[user.ID]
 	if result == nil {
 		return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.ID))
@@ -30,14 +40,35 @@ func (user *User) Get() *errors.RestErr {
 }
 
 func (user *User) Save() *errors.RestErr {
-	current := userDB[user.ID]
-	if current != nil {
-		if current.Email == user.Email {
-			return errors.NewBadRequestError(fmt.Sprintf("email %s already registered", user.Email))
-		}
-		return errors.NewBadRequestError(fmt.Sprintf("user %d already exists", user.ID))
+	// A statement is a connection so it's necessary to close it after use it
+	stmt, err := users_db.Client.Prepare(queryInsertUser)
+	// Check if it was an error preparin the statement
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
 	}
+	defer stmt.Close()
+
 	user.DateCreated = date_utils.GetNowString()
-	userDB[user.ID] = user
+
+	insertResult, err := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
+	if err != nil {
+		if strings.Contains(err.Error(), indexUniqueEmail) {
+			return errors.NewBadRequestError(fmt.Sprintf("email %s already exists", user.Email))
+		}
+		return errors.NewInternalServerError(fmt.Sprintf("error when trying to save user: %s", err.Error()))
+	}
+
+	/*
+	 * Another way to do an insert is just pass the query with the values
+	 * insertResult, err := stmt.Exec(queryInsertUser, user.FirstName, user.LastName, user.Email, user.DateCreated)
+	 * Fede León says that the 1st way is better because Prepare() method
+	 * validates that the query is a valid query and performs better.
+	 */
+
+	userID, err := insertResult.LastInsertId()
+	if err != nil {
+		return errors.NewInternalServerError(fmt.Sprintf("error when thying to save user: %s", err.Error()))
+	}
+	user.ID = userID
 	return nil
 }
