89271f4931eaba4575a2119b8a9031176fe36f27
samirgattas <samirgattas@gmail.com>
2022-11-23 22:21:19 -0300

    Load environment variables from a file

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "github.com/develop-microservices-in-go/bookstore_users-api/app"
+import (
+	"log"
+
+	"github.com/develop-microservices-in-go/bookstore_users-api/app"
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	err := godotenv.Load("local.env")
+	if err != nil {
+		log.Fatalf("Some error occured. Err: %s", err)
+	}
 	app.StartApplication()
 }
